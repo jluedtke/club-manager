bb0b19bd24a83366527bb62d0c5449e7ee174f90
Jared Luedtke <jared7223@hotmail.com>
2017-07-14 08:48:59 -0700

    fix bug that didnt install all of angular's core files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Church of Dwayne "the Rock" Johnson';
 }
